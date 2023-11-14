e223320310006c4f43c95bf39b3f74f6c9dfe7bf
chideraemmanuel <120053997+chideraemmanuel@users.noreply.github.com>
2023-11-14 16:09:36 +0100

    feat: adds route handlers for cart item increment and decrement, and cart item remove

--- src/controllers/cart.ts
+++ src/controllers/cart.ts
@@ -65,13 +65,15 @@ export const addToCart = async (
     const productExists = await Product.findById(product);
 
     if (!productExists) {
-      return response.status(404).json({ error: 'Product does not exist' });
+      return response
+        .status(404)
+        .json({ error: 'Product with the supplied ID does not exist' });
     }
 
     try {
-      const cart = await Cart.findOne({ user: user._id });
+      const userCart = await Cart.findOne({ user: user._id });
 
-      if (!cart) {
+      if (!userCart) {
         try {
           const newCart = await Cart.create({
             user: user._id,
@@ -91,31 +93,14 @@ export const addToCart = async (
       }
 
       // check if item is already in cart
-      const cart_items = cart?.cart_items;
-      console.log(cart_items);
+      const cart_items = userCart?.cart_items;
 
       const itemExistsInCart = cart_items?.find((item) => {
-        // console.log('item.product', item.product);
-        // console.log('product', product);
         return item.product.equals(product);
       });
 
       if (itemExistsInCart) {
         return response.status(400).json({ error: 'Product already in cart' });
-
-        // TODO: probably add increment functionality in this same route
-
-        // const updatedCart = await Cart.findOneAndUpdate(
-        //   { user: user._id },
-        //   {
-        //     $set: { $inc: { 'cart_items.$[element].quantity': 1 } },
-        //   },
-        //   {
-        //     new: true,
-        //     arrayFilters: [{ 'element.product': product }],
-        //   }
-        // );
-        // return response.status(200).json(updatedCart);
       }
 
       try {
@@ -176,3 +161,259 @@ export const addToCart = async (
 //   console.log('[CART_FETCH_ERROR]', error);
 //   return response.status(500).json({ error: 'Internal Server Error' });
 // }
+
+export const incrementItem = async (
+  request: express.Request,
+  response: express.Response
+) => {
+  // @ts-ignore
+  const user = request.user;
+  const { product } = request.body;
+
+  if (!product) {
+    return response
+      .status(400)
+      .json({ error: 'Please supply a product to increment' });
+  }
+
+  if (!mongoose.isValidObjectId(product)) {
+    return response.status(400).json({ error: 'Invalid product ID' });
+  }
+
+  try {
+    const productExists = await Product.findById(product);
+
+    if (!productExists) {
+      return response
+        .status(404)
+        .json({ error: 'Product with the supplied ID does not exist' });
+    }
+
+    try {
+      const userCart = await Cart.findOne({ user: user._id });
+
+      if (!userCart) {
+        try {
+          await Cart.create({
+            user: user._id,
+            cart_items: [],
+          });
+          return response.status(400).json({ error: 'Item is not in cart' });
+        } catch (error: any) {
+          console.log('[CART_CREATION_ERROR]', error);
+          return response.status(500).json({ error: 'Internal Server Error' });
+        }
+      }
+
+      const { cart_items } = userCart;
+
+      const itemExistsInCart = cart_items.find((item) =>
+        item.product.equals(product)
+      );
+
+      if (!itemExistsInCart) {
+        // return response.status(400).json({ error: 'Item is not in cart'})
+        try {
+          const updatedCart = await Cart.findOneAndUpdate(
+            { user: user._id },
+            { $push: { cart_items: { product, quantity: 1 } } },
+            { new: true }
+          );
+
+          return response.status(200).json(updatedCart);
+        } catch (error: any) {
+          console.log('[CART_UPDATE_ERROR]', error);
+          return response.status(500).json({ error: 'Internal Server Error' });
+        }
+      }
+
+      // increment item here!
+      const updatedCart = await Cart.findOneAndUpdate(
+        { user: user._id },
+        {
+          $inc: { 'cart_items.$[element].quantity': 1 },
+        },
+        {
+          new: true,
+          arrayFilters: [{ 'element.product': product }],
+        }
+      );
+
+      return response.status(200).json(updatedCart);
+    } catch (error: any) {
+      console.log('[CART_FETCH_ERROR]', error);
+      return response.status(500).json({ error: 'Internal Server Error' });
+    }
+  } catch (error: any) {
+    console.log('[PRODUCT_FETCH_ERROR]', error);
+    return response.status(500).json({ error: 'Internal Server Error' });
+  }
+};
+
+export const decrementItem = async (
+  request: express.Request,
+  response: express.Response
+) => {
+  // @ts-ignore
+  const user = request.user;
+  const { product } = request.body;
+
+  if (!product) {
+    return response
+      .status(400)
+      .json({ error: 'Please supply a product to decrement' });
+  }
+
+  if (!mongoose.isValidObjectId(product)) {
+    return response.status(400).json({ error: 'Invalid product ID' });
+  }
+
+  try {
+    const productExists = await Product.findById(product);
+
+    if (!productExists) {
+      return response
+        .status(404)
+        .json({ error: 'Product with the supplied ID does not exist' });
+    }
+
+    try {
+      const userCart = await Cart.findOne({ user: user._id });
+
+      if (!userCart) {
+        try {
+          await Cart.create({
+            user: user._id,
+            cart_items: [],
+          });
+          return response.status(400).json({ error: 'Item is not in cart' });
+        } catch (error: any) {
+          console.log('[CART_CREATION_ERROR]', error);
+          return response.status(500).json({ error: 'Internal Server Error' });
+        }
+      }
+
+      const { cart_items } = userCart;
+
+      const itemExistsInCart = cart_items.find((item) =>
+        item.product.equals(product)
+      );
+
+      if (!itemExistsInCart) {
+        return response.status(400).json({ error: 'Item is not in cart' });
+      }
+
+      if (itemExistsInCart.quantity === 1) {
+        // remove item from cart
+        const updatedCart = await Cart.findOneAndUpdate(
+          { user: user._id },
+          {
+            $pull: { cart_items: { product } },
+          },
+          {
+            new: true,
+            // arrayFilters: [{ 'element.product': product }],
+          }
+        );
+
+        return response.status(200).json(updatedCart);
+      }
+
+      // decrement item here!
+      const updatedCart = await Cart.findOneAndUpdate(
+        { user: user._id },
+        {
+          $inc: { 'cart_items.$[element].quantity': -1 },
+        },
+        {
+          new: true,
+          arrayFilters: [{ 'element.product': product }],
+        }
+      );
+
+      return response.status(200).json(updatedCart);
+    } catch (error: any) {
+      console.log('[CART_FETCH_ERROR]', error);
+      return response.status(500).json({ error: 'Internal Server Error' });
+    }
+  } catch (error: any) {
+    console.log('[PRODUCT_FETCH_ERROR]', error);
+    return response.status(500).json({ error: 'Internal Server Error' });
+  }
+};
+
+export const removeFromCart = async (
+  request: express.Request,
+  response: express.Response
+) => {
+  // @ts-ignore
+  const user = request.user;
+  const { product } = request.body;
+
+  if (!product) {
+    return response
+      .status(400)
+      .json({ error: 'Please supply a product to remove from cart' });
+  }
+
+  if (!mongoose.isValidObjectId(product)) {
+    return response.status(400).json({ error: 'Invalid product ID' });
+  }
+
+  try {
+    const productExists = await Product.findById(product);
+
+    if (!productExists) {
+      return response
+        .status(400)
+        .json({ error: 'Product with the supplied ID does not exist' });
+    }
+
+    try {
+      // remove from cart
+      const userCart = await Cart.findOne({ user: user._id });
+
+      if (!userCart) {
+        try {
+          await Cart.create({
+            user: user._id,
+            cart_items: [],
+          });
+          return response.status(400).json({ error: 'Item is not in cart' });
+        } catch (error: any) {
+          console.log('[CART_CREATION_ERROR]', error);
+          return response.status(500).json({ error: 'Internal Server Error' });
+        }
+      }
+
+      const { cart_items } = userCart;
+
+      const itemExistsInCart = cart_items.find((item) =>
+        item.product.equals(product)
+      );
+
+      if (!itemExistsInCart) {
+        return response.status(400).json({ error: 'Item is not in cart' });
+      }
+
+      // remove item from cart
+      const updatedCart = await Cart.findOneAndUpdate(
+        { user: user._id },
+        {
+          $pull: { cart_items: { product } },
+        },
+        {
+          new: true,
+          // arrayFilters: [{ 'element.product': product }],
+        }
+      );
+
+      return response.status(200).json(updatedCart);
+    } catch (error: any) {
+      console.log('[CART_FETCH_ERROR]', error);
+    }
+  } catch (error: any) {
+    console.log('[PRODUCT_FETCH_ERROR]', error);
+    return response.status(500).json({ error: 'Internal Server Error' });
+  }
+};
--- src/routes/cart.ts
+++ src/routes/cart.ts
@@ -1,9 +1,18 @@
 import express from 'express';
-import { addToCart, getUserCart } from '../controllers/cart';
+import {
+  addToCart,
+  decrementItem,
+  getUserCart,
+  incrementItem,
+  removeFromCart,
+} from '../controllers/cart';
 
 const router = express.Router();
 
 router.get('/', getUserCart);
 router.post('/', addToCart);
+router.delete('/', removeFromCart);
+router.put('/increment', incrementItem);
+router.put('/decrement', decrementItem);
 
 export default router;
